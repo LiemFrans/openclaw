bae2414124b44bfe6b6fc20f5feaf24e4f80f94b
Fransiskus Cahyadi <fransiskus.cahyadi@gmail.com>
2026-04-09 22:44:40 +0700

    chatwoot: add setup, entry points, and runtime barrel

---
+++ extensions/chatwoot/api.ts
@@ -0,0 +1,4 @@
+export { chatwootPlugin } from "./src/channel.js";
+export { setChatwootRuntime } from "./src/runtime.js";
+export * from "./src/accounts.js";
+export * from "./src/setup-surface.js";
