caacfe8d0bb050fbe115f153283eefb501d02cf5
Fransiskus Cahyadi <fransiskus.cahyadi@gmail.com>
2026-05-11 11:23:49 +0700

    chatwoot: use explicit api exports

--- extensions/chatwoot/api.ts
+++ extensions/chatwoot/api.ts
@@ -1,4 +1,11 @@
 export { chatwootPlugin } from "./src/channel.js";
 export { setChatwootRuntime } from "./src/runtime.js";
-export * from "./src/accounts.js";
-export * from "./src/setup-surface.js";
+export {
+  DEFAULT_ACCOUNT_ID,
+  isChatwootConfigured,
+  normalizeChatwootBaseUrl,
+  resolveChatwootAccount,
+  resolveChatwootAccountWithSecrets,
+} from "./src/accounts.js";
+export type { ResolvedChatwootAccount } from "./src/accounts.js";
+export { chatwootSetupWizard } from "./src/setup-surface.js";
